f5badcce8aca418fadb4223ae44fd445ad624388
fermaud <robin.fermaud@1d-lab.eu>
2019-04-29 13:57:56 +0200

    add api rate limit

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 var serveStatic = require('serve-static');
 const app = express();
-
+app.enable('trust proxy');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
 app.use(sslRedirect(['production'], 301));
 app.use(serveStatic(path.join(__dirname, 'dist')));
-
+app.use(limiter);
 app.use(morgan('combined'));
 
 app.use(bodyParser.json());
